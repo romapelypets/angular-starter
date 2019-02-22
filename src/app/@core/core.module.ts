8e33c88dba9e7d08f22c00fc92b8b2a6bb8893e2
Roman Pelypets <pelypets.ri@gmail.com>
2019-02-22 22:17:27 +0200

    Initital commit

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,13 +1,12 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 
-
 @NgModule({
   declarations: [],
   imports: [],
   providers: []
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+  // constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     // throwIfAlreadyLoaded(parentModule, 'CoreModule');
-  }
+  // }
 }
