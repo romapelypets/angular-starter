a6c5c076d7e5402b3881809fa9a6382dbe0b6dd3
Roman Pelypets <pelypets.ri@gmail.com>
2019-02-26 23:32:50 +0200

    Fix prerender build

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,14 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: {  server: './server.ts' },
+  entry: {  
+    server: './server.ts',
+    prerender: './prerender.ts'
+  },
   resolve: { extensions: ['.js', '.ts'] },
   target: 'node',
   mode: 'none',
   // this makes sure we include node_modules and other 3rd party libraries
   externals: [/(node_modules|main\..*\.js)/],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname),
     filename: '[name].js'
   },
   module: {
@@ -18,8 +21,6 @@ module.exports = {
     ]
   },
   plugins: [
-    // Temporary Fix for issue: https://github.com/angular/angular/issues/11580
-    // for "WARNING Critical dependency: the request of a dependency is an expression"
     new webpack.ContextReplacementPlugin(
       /(.+)?angular(\\|\/)core(.+)?/,
       path.join(__dirname, 'src'), // location of your src
